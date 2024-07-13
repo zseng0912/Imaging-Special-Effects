628087a069e33e40f2c295803ee93ff6787d84f5
Tan Zu Seng <141992454+zseng0912@users.noreply.github.com>
2024-07-13 13:28:19 +0800

    Update adjustColorBalance.py

--- adjustColorBalance.py
+++ adjustColorBalance.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+#Read an image using OpenCV
+img = cv2.imread('input_image.jpg')
+
+#Adjust color balance by changing channel intensities
+blue, green, red = cv2.split(img)
+
+# Example: Increase red color
+red_adjusted = cv2.addWeighted(red, 1.5, 0, 0, 0)
+
+#Merge the channels back together
+adjusted_img = cv2.merge((blue, green, red_adjusted)）
+
+cv2.imshow('Color Balance Adjusted', adjusted_img)
